429fd9b21a276c4c6b29d2df91cc4be3c3034925
xnio94 <xnio94@gmail.com>
2018-12-14 22:59:34 +0100

    dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Simple Calculator'),
+      home: MyHomePage(title: 'SimpleCalculator'),
     );
   }
 }
