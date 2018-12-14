a2395a6e53e6eb09e1e2a74b4c54b974d9b64861
xnio94 <xnio94@gmail.com>
2018-12-15 00:10:54 +0100

    adding maths logic

--- lib/main.dart
+++ lib/main.dart
@@ -38,27 +38,75 @@ class MyButton extends RaisedButton {
 */
 
 class MyButton extends Tooltip {
-  MyButton(x, context)
+  MyButton(x, f, context)
       : super(
             message: x,
             child: new RaisedButton(
-              child: Text(x),
+              child: Text(x,
+                  style: TextStyle(fontSize: 30, color: Color(0xFFFFFFFF))),
               color: Theme.of(context).accentColor,
               splashColor: Colors.blueGrey,
-              onPressed: () {
-                print("anas");
-
-
-              },
+              onPressed: f,
             ));
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int NUM = 0;
+  double num = 0;
+  double res = 0;
   String op;
+  void pressNum(x) {
+    setState(() {
+      num *= 10;
+      num += x;
+    });
+  }
 
-  void _incrementCounter() {
-    setState(() {});
+  void pressOpp(x) {
+    setState(() {
+      if (op == null)
+        res = num;
+      else {
+        if (op == "*") res = res * num;
+        if (op == "/") {
+          if (num != 0)
+            num = res / res;
+          else
+            print("problem : no devid by 0 ");
+        }
+        if (op == "+") res = res + num;
+        if (op == "-") res = res - num;
+        op = null;
+      }
+
+      num = 0;
+      op = x;
+    });
+  }
+
+  void pressEql() {
+    setState(() {
+      if (op == null)
+        res = num;
+      else {
+        if (op == "*") num = res * num;
+        if (op == "/") {
+          if (num != 0)
+            num = res / num;
+          else
+            print("problem : no devid by 0 ");
+        }
+        if (op == "+") num = res + num;
+        if (op == "-") num = res - num;
+        op = null;
+      }
+    });
+  }
+
+  void pressC() {
+    setState(() {
+      num = 0;
+      res = 0;
+    });
   }
 
   @override
@@ -73,43 +121,59 @@ class _MyHomePageState extends State<MyHomePage> {
           new Row /*or Column*/ (
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              new Icon(Icons.star, size: 100.0),
+              //new Icon(Icons.star, size: 100.0),
+              Container(
+                  // color: Colors.blueAccent,
+                  height: 80,
+                  child: Center(
+                    child: Text(
+                      (num == num.floor())
+                          ? num.floor().toString()
+                          : num.toString(),
+                      textAlign: TextAlign.left,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ), //color: Colors.white),
+                    ),
+                  ))
             ],
           ),
           new Row /*or Column*/ (
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              MyButton('1', context),
-              MyButton('2', context),
-              MyButton('3', context),
-              MyButton('+', context),
+              MyButton('1', () => pressNum(1), context),
+              MyButton('2', () => pressNum(2), context),
+              MyButton('3', () => pressNum(3), context),
+              MyButton('+', () => pressOpp('+'), context),
             ],
           ),
           new Row /*or Column*/ (
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              MyButton('4', context),
-              MyButton('5', context),
-              MyButton('6', context),
-              MyButton('-', context)
+              MyButton('4', () => pressNum(4), context),
+              MyButton('5', () => pressNum(5), context),
+              MyButton('6', () => pressNum(6), context),
+              MyButton('-', () => pressOpp('-'), context)
             ],
           ),
           new Row /*or Column*/ (
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              MyButton('7', context),
-              MyButton('8', context),
-              MyButton('9', context),
-              MyButton('*', context),
+              MyButton('7', () => pressNum(7), context),
+              MyButton('8', () => pressNum(8), context),
+              MyButton('9', () => pressNum(9), context),
+              MyButton('*', () => pressOpp('*'), context),
             ],
           ),
           new Row /*or Column*/ (
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              MyButton('C', context),
-              MyButton('0', context),
-              MyButton('=', context),
-              MyButton('/', context),
+              MyButton('C', pressC, context),
+              MyButton('0', () => pressNum(0), context),
+              MyButton('=', pressEql, context),
+              MyButton('/', () => pressOpp('/'), context),
             ],
           ),
         ],
